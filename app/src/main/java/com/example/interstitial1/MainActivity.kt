951eda19a188acf8d75a0336454524872ab2a282
Dilshan <tdm.dev2@gmail.com>
2022-10-09 20:27:47 +0530

    Initial commit

---
+++ app/src/main/java/com/example/interstitial1/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.interstitial1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var mAdView : AdView
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        MobileAds.initialize(this) {}
+
+        mAdView = findViewById(R.id.ad_view)
+        val adRequest = AdRequest.Builder().build()
+        mAdView.loadAd(adRequest)
+
+    }
+}
